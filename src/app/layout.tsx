023a7e9a46c034a3218059ae97d550605315d1cc
arjunbellur <arjunbellur@outlook.com>
2025-09-05 19:59:44 -0400

    Change site font to Futura with Roboto backup - add Google Fonts import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
